b2fae39cd979f3be7d83988ff52b54ea9e5a1c17
pablocerrutti <pablocerrutti@gmail.com>
2026-09-21 15:17:04 -0300

    Fix Cornerstone3D optional PolySeg build dependency

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,21 @@
 import { defineConfig } from 'vite';
-export default defineConfig({base:'./',optimizeDeps:{exclude:['@cornerstonejs/dicom-image-loader'],include:['dicom-parser']},worker:{format:'es'},build:{target:'es2020'}});
+
+export default defineConfig({
+  base: './',
+  optimizeDeps: {
+    exclude: ['@cornerstonejs/dicom-image-loader', '@icr/polyseg-wasm'],
+    include: ['dicom-parser']
+  },
+  worker: {
+    format: 'es',
+    rollupOptions: {
+      external: ['@icr/polyseg-wasm']
+    }
+  },
+  build: {
+    target: 'es2020',
+    rollupOptions: {
+      external: ['@icr/polyseg-wasm']
+    }
+  }
+});
